609adb3660365f57eb0703c82f65041d394e1a37
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-01-12 11:34:05 -0300

    feat: add UserServiceComplete

--- src/main/java/daniel/services/impl/UserServiceImpl.java
+++ src/main/java/daniel/services/impl/UserServiceImpl.java
@@ -2,18 +2,23 @@ package daniel.services.impl;
 
 import daniel.entities.UserEntity;
 import daniel.repositories.UserRepository;
+import daniel.services.TokenService;
 import daniel.services.UserService;
 import daniel.utils.dtos.users.ResponseToken;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class UserServiceImpl implements UserService {
+
     private final UserRepository repository;
+    private final TokenService tokenService;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public void registerUser(UserEntity data) {
@@ -23,19 +28,19 @@ public class UserServiceImpl implements UserService {
     @Override
     public ResponseToken loginUser(UserEntity data) {
         log.info("Login attempt: {}", data.getName());
-        UserEntity adminEntity = repository.findByName(data.getName())
+        UserEntity user = repository.findByName(data.getName())
                 .orElseThrow(() -> {
-                    log.error("User not found: {}", data.getEmail());
-                    return new EntityNotFoundException("User with email " + data.getEmail() + " not found");
+                    log.error("User not found: {}", data.getName());
+                    return new EntityNotFoundException("User with email " + data.getName() + " not found");
                 });
 
-        if (!this.passwordEncoder.matches(data.getPassword(), adminEntity.getPassword())) {
-            log.error("Incorrect password for user {}", adminEntity.getEmail());
+        if (!this.passwordEncoder.matches(data.getPassword(), user.getPassword())) {
+            log.error("Incorrect password for user {}", user.getName());
             throw new IllegalArgumentException("Invalid email or password");
         }
 
-        repository.save(adminEntity);
-        log.info("User {} successfully verified.", adminEntity.getEmail());
+        String token = tokenService.generateToken(user);
+        log.info("User {} successfully verified.", user.getName());
 
         return new ResponseToken(token);
     }
